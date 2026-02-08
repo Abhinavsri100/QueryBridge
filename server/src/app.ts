c33ea9c97f742dc967739d45a5c9302eace73676
Admin <admin@macOS-Abhinav-Srivastava-G6H9R99MV0.local>
2026-02-08 16:59:00 +0530

    added frontned url to cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 // 2. CORS
 app.use(cors({
-  origin: "http://query-bridge.vercel.app/",
+  origin: ["http://query-bridge.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
